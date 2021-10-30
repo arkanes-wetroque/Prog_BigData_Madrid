4834543f6607fc816e85731ba5f0a9180ea05495
Dilhan BERNARD <d.bernard@alumnos.upm.es>
2021-10-30 18:40:34 +0200

    Dash generation test 1

--- main.py
+++ main.py
@@ -2,22 +2,20 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
+import dash_bootstrap_components as dbc
+from dash import Input, Output, dcc, html
+
 import pandas as pd
 
 from datasVehicules import TotalConsoByTypo
 from functions import generate_table
 
-app = dash.Dash(__name__)
+
+
+#VARIABLES
+from pages import pageTypeConso
 
 """
-Phase de test inportation de la conso dataframe et insertion dans un grapgique
-dash
-"""
-testDF = TotalConsoByTypo()
-
-
-figureTest = px.bar(testDF,x= "Type", y=["2017", "2018", "2019"], barmode="group")
-
 
 app.layout = html.Div(children=[
     html.H1(children='test'),
@@ -28,17 +26,77 @@ app.layout = html.Div(children=[
     html.Div(children=[
         dcc.Graph(
         id='test-graph',
-        figure=figureTest
+        figure=figureConsoTotal
     ),
     html.H4(children='Datas by types'),
-    generate_table(testDF)
-    ])
-
-
-
-
-
+    generate_table(DFConsoTypeTotal)
+    ],),
 ])
+
+"""
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("Sidebar", className="display-4"),
+        html.Hr(),
+        html.P(
+            "A simple sidebar layout with navigation links", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Tipos", href="/", active="exact"),
+                dbc.NavLink("Page 1", href="/page-1", active="exact"),
+                dbc.NavLink("Page 2", href="/page-2", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+
+
+
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return pageTypeConso()
+    elif pathname == "/page-1":
+        return html.P("This is the content of page 1. Yay!")
+    elif pathname == "/page-2":
+        return html.P("Oh cool, this is page 2!")
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
